b5979162590943f368c9359f59db37546c989412
JM_command <jaime00coelho@gmail.com>
2023-12-06 20:47:41 +0100

    Initial commit

---
+++ src/main/java/ch/jmcommand/mahtimer/MAH_Timer.java
@@ -0,0 +1,17 @@
+package ch.jmcommand.mahtimer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MAH_Timer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
